f35ea1692d058f51b76ff676ad97b874ae57739c
Liam Hatcher <hatcherpiano@gmail.com>
2016-06-10 21:18:55 -0700

    Install redux

--- IllBeThereForYou/client/App.jsx
+++ IllBeThereForYou/client/App.jsx
@@ -11,7 +11,7 @@ class App extends React.Component {
   }
 
   render() {
-    return <h1>Yo</h1> 
+    return <h1>Hello</h1> 
   }
 }
 
--- IllBeThereForYou/client/main.js
+++ IllBeThereForYou/client/main.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReduxPromise from 'redux-promise';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';  
+import { applyMiddleware } from 'redux';
 
+import appReducer from './reducers/appReducer';
 import App from './App';
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+let store = createStoreWithMiddleware(appReducer);
 
 const root = document.getElementById('app');
 
-ReactDOM.render(<App/>, root);  
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider> , 
+root);  
